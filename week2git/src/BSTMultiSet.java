7d817d35b05205c8d04c3f61470c2afba5d52ea3
Samir Hendawi <samirhendawi@Samirs-MacBook-Pro-2.local>
2024-09-18 21:42:31 -0400

    added BST Multiset

--- week2git/src/BSTMultiSet.java
+++ week2git/src/BSTMultiSet.java
@@ -4,9 +4,36 @@ public class BSTMultiSet extends MultiSet {
     // here; no need to explicitly write a new constructor.
     private final BST bst = new BST();
 
-    // TODO Task: hover the red squiggly on the first line and select 'Implement methods'.
-    //            All listed methods should be selected. Press okay and then implement each
-    //            method. As with the python version, this shouldn't require a lot of code to write.
+    @Override
+    void add(Integer item) {
+        bst.insert(item);
+    }
+
+    @Override
+    void remove(Integer item) {
+        bst.delete(item);
+    }
+
+    @Override
+    boolean contains(Integer item) {
+        return bst.contains(item);
+    }
+
+    @Override
+    boolean isEmpty() {
+        return bst.isEmpty();
+    }
+
+    @Override
+    int count(Integer item) {
+        return bst.count(item);
+    }
+
+    @Override
+    int size() {
+        return bst.getLength();
+    }
+
     //
     //            Hint: autocomplete will help as you look for the right bst methods to call,
     //                  but make sure to double-check that you are calling the correct methods!
